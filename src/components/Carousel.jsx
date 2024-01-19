3f648732697daf56c8b2b40ac64a1cafa794afb3
Dhanarooban1 <dhanazx1@gmail.com>
2024-01-19 09:56:57 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
+
 function Carousel() {
+    let [count, setCount] = useState(0);
+
+    function NavigationFunction(mode) {
+        setCount((prev) => (mode === "left" ? (prev - 1 + images.length) % images.length : (prev + 1 ) % images.length));
+      }
+
+
+return(
+<>
+   <div id="body" style={ {backgroundImage: `url(${images[count].img})`} }>
+        <div className="Arrow"> <ArrowBackIosIcon style={{ color: "white", fontSize: "40px" }} onClick={() => NavigationFunction("left")}/>
+        </div>
+        <div id="textContent">
+            <h2 id="txt">{images[count].title}</h2>
+            <h2 id="txt">{images[count].subtitle}</h2>
+        </div>
+        <div className="Arrow">
+        <ArrowForwardIosIcon style={{ color: "white", fontSize: "40px" }} onClick={() => NavigationFunction("right")}/>
+        </div>
+    </div>
+</>
+   )
 }
+
+
+
+
+
+
 
 export default Carousel;
